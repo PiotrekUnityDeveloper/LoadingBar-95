4925ca480c0f1eb766a41d4b8f64252060dca9e8
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2022-02-07 15:53:49 +0100

    Almost Complete Summary system

--- Assets/Scripts/TileCatcher.cs
+++ Assets/Scripts/TileCatcher.cs
@@ -7,6 +7,11 @@ public class TileCatcher : MonoBehaviour
 {
     public int procent = 0; //%
 
+    public Slider bluepoints;
+    public Slider orangepoints;
+
+    [Header("Tiles")]
+
     //tile simulators
     public GameObject tile1;
     public GameObject tile2;
@@ -29,6 +34,35 @@ public class TileCatcher : MonoBehaviour
     public GameObject tile19;
     public GameObject tile20;
 
+    //Graph Stuff
+    public Image graphimg;
+
+    //Graph Textures
+    #region
+    public Sprite blue0;
+    public Sprite blue5;
+    public Sprite blue10;
+    public Sprite blue15;
+    public Sprite blue20;
+    public Sprite blue25;
+    public Sprite blue30;
+    public Sprite blue35;
+    public Sprite blue40;
+    public Sprite blue45;
+    public Sprite blue50;
+    public Sprite blue55;
+    public Sprite blue60;
+    public Sprite blue65;
+    public Sprite blue70;
+    public Sprite blue75;
+    public Sprite blue80;
+    public Sprite blue85;
+    public Sprite blue90;
+    public Sprite blue95;
+    public Sprite blue100;
+    #endregion
+
+
     //Animations
     public GameObject Instantiator;
     public GameObject AnimatedTile;
@@ -38,7 +72,44 @@ public class TileCatcher : MonoBehaviour
     public Text procentTextShadow;
     public Text secondProcentShadow;
 
+    //final scoring
+    private int BlueTiles;
+    private int OrangeTiles;
 
+    #region uistuff
+    //final ui rendering stuff
+
+    public Image finaltile1;
+    public Image finaltile2;
+    public Image finaltile3;
+    public Image finaltile4;
+    public Image finaltile5;
+    public Image finaltile6;
+    public Image finaltile7;
+    public Image finaltile8;
+    public Image finaltile9;
+    public Image finaltile10;
+    public Image finaltile11;
+    public Image finaltile12;
+    public Image finaltile13;
+    public Image finaltile14;
+    public Image finaltile15;
+    public Image finaltile16;
+    public Image finaltile17;
+    public Image finaltile18;
+    public Image finaltile19;
+    public Image finaltile20;
+
+    #endregion uistuff
+
+    public Color bluecolor;
+    public Color orangecolor;
+
+    private void Awake()
+    {
+        bluecolor = new Color(22, 0, 209);
+        orangecolor = new Color(255, 179, 0);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -698,5 +769,312 @@ public class TileCatcher : MonoBehaviour
         yield return new WaitForSecondsRealtime(0.2f);
 
         StartCoroutine(HundredProcentDelayChecker());
+    }
+
+    public void ScoreCounter()
+    {
+        if(tile1.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile1.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile1.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile2.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile2.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile2.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile3.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile3.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile3.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile4.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile4.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile4.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile5.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile5.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile5.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile5.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile5.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile5.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile6.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile6.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile6.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile7.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile7.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile7.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile8.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile8.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile8.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile9.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile9.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile9.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile10.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile10.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile10.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile11.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile11.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile11.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile12.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile12.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile12.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile13.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile13.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile13.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile14.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile14.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile14.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile15.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile15.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile15.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile16.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile16.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile16.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile17.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile17.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile17.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile18.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile18.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile18.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile19.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile19.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile19.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        if (tile20.GetComponent<SpriteRenderer>().color == new Color(255, 179, 0)) //if its orange
+        {
+            finaltile20.GetComponent<Image>().color = orangecolor;
+            OrangeTiles++;
+        }
+        else //if not then its blue
+        {
+            finaltile20.GetComponent<Image>().color = bluecolor;
+            BlueTiles++;
+        }
+
+        bluepoints.value = BlueTiles * 5;
+        orangepoints.value = OrangeTiles * 5;
+
+        //change graph's texture here
+
+        switch(bluepoints.value)
+        {
+            case 0:
+                graphimg.GetComponent<Image>().sprite = blue0;
+                break;
+            case 5:
+                graphimg.GetComponent<Image>().sprite = blue5;
+                break;
+            case 10:
+                graphimg.GetComponent<Image>().sprite = blue10;
+                break;
+            case 15:
+                graphimg.GetComponent<Image>().sprite = blue15;
+                break;
+            case 20:
+                graphimg.GetComponent<Image>().sprite = blue20;
+                break;
+            case 25:
+                graphimg.GetComponent<Image>().sprite = blue25;
+                break;
+            case 30:
+                graphimg.GetComponent<Image>().sprite = blue30;
+                break;
+            case 35:
+                graphimg.GetComponent<Image>().sprite = blue35;
+                break;
+            case 40:
+                graphimg.GetComponent<Image>().sprite = blue40;
+                break;
+            case 45:
+                graphimg.GetComponent<Image>().sprite = blue45;
+                break;
+            case 50:
+                graphimg.GetComponent<Image>().sprite = blue50;
+                break;
+            case 55:
+                graphimg.GetComponent<Image>().sprite = blue55;
+                break;
+            case 60:
+                graphimg.GetComponent<Image>().sprite = blue60;
+                break;
+            case 65:
+                graphimg.GetComponent<Image>().sprite = blue65;
+                break;
+            case 70:
+                graphimg.GetComponent<Image>().sprite = blue70;
+                break;
+            case 75:
+                graphimg.GetComponent<Image>().sprite = blue75;
+                break;
+            case 80:
+                graphimg.GetComponent<Image>().sprite = blue80;
+                break;
+            case 85:
+                graphimg.GetComponent<Image>().sprite = blue85;
+                break;
+            case 90:
+                graphimg.GetComponent<Image>().sprite = blue90;
+                break;
+            case 95:
+                graphimg.GetComponent<Image>().sprite = blue95;
+                break;
+            case 100:
+                graphimg.GetComponent<Image>().sprite = blue100;
+                break;
+
+        }
     }
 }
