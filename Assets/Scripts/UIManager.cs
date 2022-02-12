a6da0d17ee38950129ddd498b0dff6a1edfdafbc
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2022-02-12 14:42:50 +0100

    ui script + new scene + better UI (message dialogs)

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    //ingame
+    
+
+    //inlobby (os-menu)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
