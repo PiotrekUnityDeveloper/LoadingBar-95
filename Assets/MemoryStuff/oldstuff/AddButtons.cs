c4f5d798ce62d3bfffe3f9db99f8c29dff258548
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2022-02-14 14:01:56 +0100

    removed scene that crashes unity

---
+++ Assets/MemoryStuff/oldstuff/AddButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButtons : MonoBehaviour
+{
+    [SerializeField]
+    private Transform gameField;
+    // alternative for GetComponent
+
+    [SerializeField]
+    private GameObject btn;
+
+    void Awake()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(gameField, false);
+        }
+    }
+}
